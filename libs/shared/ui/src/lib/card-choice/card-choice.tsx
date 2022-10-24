42427b1db90872324e9e0610b2e705a2aca4b34b
Théo <43091603+glazk0@users.noreply.github.com>
2022-10-24 22:23:29 +0200

    Revert "feat: added scale animation on hover"
    This reverts commit 445110f9556b6e8d34cc1536f580bf0ae5be60a1.

--- libs/shared/ui/src/lib/card-choice/card-choice.tsx
+++ libs/shared/ui/src/lib/card-choice/card-choice.tsx
@@ -62,51 +62,48 @@ export function CardChoice(props: CardChoiceProps) {
   }, [percent, setStyle, showResult, computeStyle])
 
   return (
-    <>
+    <div
+      onClick={onClick}
+      data-testid="card"
+      className={`absolute lg-top-0 lg-bottom-0 flex items-center flex-col justify-center transition-size ease duration-1000  ${positionClass} lg:w-1/2 lg:h-full w-full h-1/2`}
+      style={style}
+    >
+      <Image
+        data-testid="card-image"
+        src={imgUrl}
+        priority
+        alt={`illustration for ${position} choice`}
+        fill
+        placeholder="empty"
+        className="relative z-0 pointer-events-none object-cover object-center"
+        sizes="(max-width: 768px) 100vw,
+              50vw"
+      />
+
+      <h1 className="px-4 text-3xl bg-black text-white uppercase font-bold w-56 text-center relative">{title}</h1>
+
       <div
-        onClick={onClick}
-        data-testid="card"
-        className={`absolute lg-top-0 lg-bottom-0 flex items-center flex-col justify-center transition-size ease duration-1000 ${positionClass} lg:w-1/2 lg:h-full w-full h-1/2 group overflow-hidden`}
-        style={style}
+        className={`relative mt-2 px-4 py-2 bg-white border-t-2 border-t-black font-bold text-black w-56 text-center opacity-0 ${
+          showResult ? '!opacity-100 transition-opacity duration-500' : ''
+        }`}
       >
-        <Image
-          data-testid="card-image"
-          src={imgUrl}
-          priority
-          alt={`illustration for ${position} choice`}
-          fill
-          placeholder="empty"
-          className={`relative -z-50 pointer-events-none object-cover object-center ${
-            !showResult ? 'scale-100 group-hover:scale-105 transition-transform duration-300 ease-in-out' : ''
-          } `}
-          sizes="(max-width: 768px) 100vw, 50vw"
-        />
-
-        <h1 className="px-4 text-3xl bg-black text-white uppercase font-bold w-56 text-center relative">{title}</h1>
-
-        <div
-          className={`relative mt-2 px-4 py-2 bg-white border-t-2 border-t-black font-bold text-black w-56 text-center opacity-0 ${
-            showResult ? '!opacity-100 transition-opacity duration-500' : ''
-          }`}
-        >
-          {showResult ? (
-            <>
-              <CountUp end={percent} duration={1} />%
-            </>
-          ) : (
-            'a'
-          )}
-        </div>
-        <div
-          className={`px-4 bg-white border-t-2 border-t-black font-bold text-black text-sm -rotate-3  opacity-0 ${
-            showResult ? '!opacity-100 delay-1000 transition-opacity duration-300' : ''
-          }`}
-        >
-          {showResult ? <>{props.voteCount} votes</> : 'a'}
-        </div>
+        {showResult ? (
+          <>
+            <CountUp end={percent} duration={1} />%
+          </>
+        ) : (
+          'a'
+        )}
+      </div>
+      <div
+        className={`px-4 bg-white border-t-2 border-t-black font-bold text-black text-sm -rotate-3  opacity-0 ${
+          showResult ? '!opacity-100 delay-1000 transition-opacity duration-300' : ''
+        }`}
+      >
+        {showResult ? <>{props.voteCount} votes</> : 'a'}
       </div>
       {props.position === 'left' && <Or showResult={props.showResult} />}
-    </>
+    </div>
   )
 }
 
