575dc43bae1911a878a6e0c40c27676e5f9fbd18
bdebon <benjamin.debon@gmail.com>
2022-11-05 23:19:23 +0100

    Revert "chore(global): pnpm replaces yarn"

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ There are also many [community plugins](https://nx.dev/community) you could add.
 Clone the repository, install the dependencies and run the project:
 
 ```bash
-  pnpm install
+  yarn install
   npx nx serve
 ```
 You should see this:
--- apps/devchoices-next/pages/question/[slug].tsx
+++ apps/devchoices-next/pages/question/[slug].tsx
@@ -6,7 +6,7 @@ import { PageTransitionWrapper, Question } from '@benjamincode/shared/ui'
 import { QuestionContext } from '../_app'
 import Image from 'next/future/image'
 
-export const getStaticProps = async (context: { params: { slug: string } }): Promise<{ props: QuestionPageProps }> => {
+export async function getStaticProps(context): Promise<{ props: QuestionPageProps }> {
   const slug = context.params.slug
   // Get external data from the file system, API, DB, etc.
   const question = questions.find((q) => q.slug === slug)
@@ -20,7 +20,7 @@ export const getStaticProps = async (context: { params: { slug: string } }): Pro
   }
 }
 
-export async function getStaticPaths() {
+export async function getStaticPaths(context) {
   return {
     paths: questions.map((q) => ({ params: { slug: q.slug } })),
     fallback: false,
@@ -44,7 +44,7 @@ export function QuestionPage(props: QuestionPageProps) {
       const currentQuestion = questionContext.questions.find((q) => q.slug === slug)
 
       let nextQuestion: QuestionInterface
-      if (currentQuestion && questionContext.questions[questionContext.questions.indexOf(currentQuestion) + 1]) {
+      if (questionContext.questions[questionContext.questions.indexOf(currentQuestion) + 1]) {
         nextQuestion = questionContext.questions[questionContext.questions.indexOf(currentQuestion) + 1]
       } else {
         nextQuestion = questionContext.questions[0]
@@ -52,7 +52,7 @@ export function QuestionPage(props: QuestionPageProps) {
 
       return nextQuestion
     }
-    return undefined
+    return null
   }, [slug, questionContext.questions])
 
   useEffect(() => {
@@ -71,14 +71,13 @@ export function QuestionPage(props: QuestionPageProps) {
   }, [slug, computeNextQuestion])
 
   const onNext = async () => {
-    await router.push(`/question/${computeNextQuestion()?.slug}`)
+    await router.push(`/question/${computeNextQuestion().slug}`)
   }
 
   const onSkip = async () => {
-    if (props.question)
-      await router.push(
-        '/question/' + questionContext.questions[questionContext.questions.indexOf(props.question) + 1].slug
-      )
+    await router.push(
+      '/question/' + questionContext.questions[questionContext.questions.indexOf(props.question) + 1].slug
+    )
   }
 
   const onLeft = () => {
@@ -95,28 +94,24 @@ export function QuestionPage(props: QuestionPageProps) {
   const NextImagesPreloader = () => {
     return (
       <div className="fixed top-[-4000px] left-[-4000px] w-[100vw] h-[50vh] lg:h-[100vh]">
-        {nextQuestion && (
-          <>
-            <Image
-              src={nextQuestion.choiceLeft.img_path}
-              alt="next image left"
-              sizes="(max-width: 768px) 100vw,
-                50vw"
-              fill
-              className="w-full h-full"
-              loading="eager"
-            />
-            <Image
-              src={nextQuestion.choiceRight.img_path}
-              alt="next image right"
-              sizes="(max-width: 768px) 100vw,
-                50vw"
-              fill
-              className="w-full h-full"
-              loading="eager"
-            />
-          </>
-        )}
+        <Image
+          src={nextQuestion.choiceLeft.img_path}
+          alt="next image left"
+          sizes="(max-width: 768px) 100vw,
+              50vw"
+          fill
+          className="w-full h-full"
+          loading="eager"
+        />
+        <Image
+          src={nextQuestion.choiceRight.img_path}
+          alt="next image right"
+          sizes="(max-width: 768px) 100vw,
+              50vw"
+          fill
+          className="w-full h-full"
+          loading="eager"
+        />
       </div>
     )
   }
@@ -124,38 +119,36 @@ export function QuestionPage(props: QuestionPageProps) {
   return (
     <PageTransitionWrapper
       className="w-full h-full absolute inset-0"
-      key={`${props.question?.choiceLeft.title}-${props.question?.choiceRight.title}`}
-      title={`${props.question?.choiceLeft.title} or ${props.question?.choiceRight.title}`}
-      description={`${props.question?.choiceLeft.title} or ${props.question?.choiceRight.title}`}
+      key={`${props.question.choiceLeft.title}-${props.question.choiceRight.title}`}
+      title={`${props.question.choiceLeft.title} or ${props.question.choiceRight.title}`}
+      description={`${props.question.choiceLeft.title} or ${props.question.choiceRight.title}`}
     >
       {nextQuestion && <NextImagesPreloader />}
-      {props.question && (
-        <Question
-          leftChoiceProps={{
-            showResult: showResult,
-            voteCount: voteValues[0],
-            imgUrl: props.question.choiceLeft.img_path,
-            position: 'left',
-            title: props.question.choiceLeft.title,
-            onClick: onLeft,
-            totalCount: voteValues[0] + voteValues[1],
-          }}
-          rightChoiceProps={{
-            showResult: showResult,
-            voteCount: voteValues[1],
-            imgUrl: props.question.choiceRight.img_path,
-            position: 'right',
-            title: props.question.choiceRight.title,
-            onClick: onRight,
-            totalCount: voteValues[0] + voteValues[1],
-          }}
-          showResult={showResult}
-          onNext={onNext}
-          onSkip={onSkip}
-          onLeft={onLeft}
-          onRight={onRight}
-        />
-      )}
+      <Question
+        leftChoiceProps={{
+          showResult: showResult,
+          voteCount: voteValues[0],
+          imgUrl: props.question?.choiceLeft.img_path,
+          position: 'left',
+          title: props.question?.choiceLeft.title,
+          onClick: onLeft,
+          totalCount: voteValues[0] + voteValues[1],
+        }}
+        rightChoiceProps={{
+          showResult: showResult,
+          voteCount: voteValues[1],
+          imgUrl: props.question?.choiceRight.img_path,
+          position: 'right',
+          title: props.question?.choiceRight.title,
+          onClick: onRight,
+          totalCount: voteValues[0] + voteValues[1],
+        }}
+        showResult={showResult}
+        onNext={onNext}
+        onSkip={onSkip}
+        onLeft={onLeft}
+        onRight={onRight}
+      />
     </PageTransitionWrapper>
   )
 }
