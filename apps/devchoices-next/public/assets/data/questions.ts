9260ece8715e7361d30c6faff1bac0b9c6d17c9d
bdebon <benjamin.debon@gmail.com>
2022-11-11 00:14:13 +0100

    generated images

--- apps/devchoices-next/public/assets/data/questions.ts
+++ apps/devchoices-next/public/assets/data/questions.ts
@@ -16,44 +16,44 @@ export const questions: QuestionInterface[] = [
     slug: 'notepad-or-emacs',
     choiceLeft: {
       title: 'Notepad',
-      img_path: '/assets/img/2.webp',
+      img_path: '/assets/img/2.jpeg',
     },
     choiceRight: {
       title: 'Emacs',
-      img_path: '/assets/img/3.webp',
+      img_path: '/assets/img/3.jpeg',
     },
   },
   {
     slug: 'svn-or-cvs',
     choiceLeft: {
       title: 'SVN',
-      img_path: '/assets/img/4.webp',
+      img_path: '/assets/img/4.jpeg',
     },
     choiceRight: {
       title: 'CVS',
-      img_path: '/assets/img/5.webp',
+      img_path: '/assets/img/5.jpeg',
     },
   },
   {
     slug: 'fortran-or-cobol',
     choiceLeft: {
       title: 'FORTRAN',
-      img_path: '/assets/img/6.webp',
+      img_path: '/assets/img/6.jpeg',
     },
     choiceRight: {
       title: 'COBOL',
-      img_path: '/assets/img/7.webp',
+      img_path: '/assets/img/7.jpeg',
     },
   },
   {
     slug: 'single-or-double-quotes',
     choiceLeft: {
       title: "'",
-      img_path: '/assets/img/single-quote.webp',
+      img_path: '/assets/img/single-quote.jpeg',
     },
     choiceRight: {
       title: '"',
-      img_path: '/assets/img/double-quotes.webp',
+      img_path: '/assets/img/double-quotes.jpeg',
     },
   },
   {
