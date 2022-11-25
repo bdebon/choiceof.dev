bf8e9ba8a16f055995aa5bde044a96e97a0c30a7
Quentin <quentin.t@gmail.com>
2022-11-25 21:15:59 +0100

    fix: use MATOMO_URL in app script tag

--- apps/devchoices-next/pages/_app.tsx
+++ apps/devchoices-next/pages/_app.tsx
@@ -105,7 +105,7 @@ function CustomApp({ Component, pageProps, router }: AppProps) {
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-          var u="//choiceof.dev/matomo/";
+          var u="${MATOMO_URL}";
           _paq.push(['setTrackerUrl', u+'matomo.php']);
           _paq.push(['setSiteId', '1']);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
